9613cdfb2f4b08111e70bfa2929dc6c9560ba2d3
Charles Dong <chardon_cs@proton.me>
2024-12-17 18:27:19 +0800

    Add doc / fix arguments

--- lua/indent-wizard/init.lua
+++ lua/indent-wizard/init.lua
@@ -1,17 +1,23 @@
 #!/usr/bin/env luajit
 
+--- @alias Settings {tabstop:uinteger|nil, softtabstop:uinteger|nil, shiftwidth:uinteger|nil, expandtab: boolean|nil, smartindent: boolean|nil}
+
+---@type table<string, Settings>
 local ft_defaults = {}
 
+--- @module "indent-wizard"
 local M = {}
 
 --- Setup function
+---
+---@param opts {auto_guess:boolean|nil, defaults:{ft:string|string[]|nil, options:Settings}[], scan:{line_count:uinteger|nil, offset:uinteger|nil}}|nil: Configuration opts
 function M.setup(opts)
   local function indent_info()
     local result = M.indent_info()
     vim.print(
       string.format(
-        "Apparently: shiftwidth=%a, expandtab=%s",
-        result.shiftwidth < 0 and "?" or result.shiftwidth,
+        "Apparently: shiftwidth=%s, expandtab=%s",
+        result.shiftwidth < 0 and "?" or tostring(result.shiftwidth),
         result.expandtab == nil and "?" or result.expandtab and "yes" or "no"
       )
     )
@@ -28,7 +34,11 @@ function M.setup(opts)
 
   vim.api.nvim_create_user_command(
     "GuessIndent",
-    function () M.set_indent(indent_info()) end,
+    function ()
+        M.set_indent({
+          scan = indent_info(),
+        })
+    end,
     {
       desc = "indent-wizard.nvim: Guess indentation for current buffer",
     }
@@ -49,6 +59,7 @@ function M.setup(opts)
         end
 
       else
+        --[=[@as (string|nil)[]]=]
         local fts = type(item.ft) == "table" and item.ft or { item.ft }
 
         for _, ft in ipairs(fts) do
@@ -66,7 +77,7 @@ function M.setup(opts)
   if opts.auto_guess == nil or opts.auto_guess then
     local group = vim.api.nvim_create_augroup("indent-wizard-auto-guess", { clear = true })
 
-    vim.api.nvim_create_autocmd("BufEnter", {
+    vim.api.nvim_create_autocmd("BufReadPost", {
       group = group,
       desc = "indent-wizard.nvim: Auto guess indentation while entering a new buffer",
       callback = function ()
@@ -92,28 +103,42 @@ function M.setup(opts)
   end
 end
 
+--- Get default indentation
+---
+--- @param ft string|nil File type (use file type from current buffer if empty)
+--- @return Settings|nil 
 function M.default_indent(ft)
   return ft_defaults[ft or vim.bo.filetype]
 end
 
+--- @alias ScanSettings {line_count:uinteger|nil, offset:number|string|nil}
+
 --- Check indentation in current buffer
+---
+--- @param opts ScanSettings|nil Indentation opts
+--- @return {shiftwidth:uinteger|nil, expandtab:boolean|nil} 
 function M.indent_info(opts)
+  opts = opts or {}
+
   local line_count = type(opts.line_count) == "number" and math.max(math.floor(opts.line_count), 1) or 20
+  --- @type number
   local offset = 0.1
   if type(opts.offset) == "number" then
     if opts.offset < 1 then
+      --[[@as number]]
       offset = opts.offset
     else
+      --[[@as number]]
       offset = math.floor(opts.offset)
     end
-  elseif type(opts.offset) == "string" and string.gmatch(opts.offset, "^%d+%%$") then
+  elseif type(opts.offset) == "string" and string.match(opts.offset, "^%d+%%$") then
     offset = tonumber(string.gsub(opts.offset, "%%", ""))
   end
 
   local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
   local length = vim.fn.len(lines)
 
-  local min_shiftwidth = nil
+  local min_shiftwidth = -1
   local expand_tab_vote = 0
 
   local abs_offset = offset < 1 and math.floor(length * offset) or offset
@@ -137,12 +162,12 @@ function M.indent_info(opts)
       break
     end
 
-    local space_len = vim.fn.len(line:gmatch("^%s+"))
+    local space_len = vim.fn.len(tostring(line:match("%s+")))
     if space_len < 1 then
       goto continue
     end
 
-    if not space_len % 2 then
+    if space_len % 2 == 0 then
       if min_shiftwidth < 0 or min_shiftwidth > space_len then
         min_shiftwidth = space_len
       end
@@ -166,34 +191,44 @@ function M.indent_info(opts)
 end
 
 --- Set indentation for current buffer
+---
+--- @param opts {global:boolean|nil, options:Settings}
 function M.set_indent(opts)
   local vim_opt = opts.global and vim.opt or vim.bo
 
-  if opts.tabstop ~= nil then
-    vim_opt.tabstop = opts.tabstop
+  if opts.options.tabstop ~= nil then
+    vim_opt.tabstop = opts.options.tabstop
   end
-  if opts.softtabstop ~= nil then
-    vim_opt.softtabstop = opts.softtabstop
+  if opts.options.softtabstop ~= nil then
+    vim_opt.softtabstop = opts.options.softtabstop
   end
-  if opts.shiftwidth ~= nil then
-    vim_opt.shiftwidth = opts.shiftwidth
+  if opts.options.shiftwidth ~= nil then
+    vim_opt.shiftwidth = opts.options.shiftwidth
   end
-  if opts.expandtab ~= nil then
-    vim_opt.expandtab = opts.expandtab
+  if opts.options.expandtab ~= nil then
+    vim_opt.expandtab = opts.options.expandtab
   end
-  if opts.smartindent ~= nil then
-    vim_opt.smartindent = opts.smartindent
+  if opts.options.smartindent ~= nil then
+    vim_opt.smartindent = opts.options.smartindent
   end
 end
 
+--- Guess indentation
+---
+--- @param opts {scan:ScanSettings|nil, fallback:Settings|nil}
 function M.guess_indent(opts)
   local fallback = opts and opts.fallback or nil
-  local indent = M.indent_info(opts.scan) or fallback
+  local indent = M.indent_info(opts.scan)
 
-  if indent then
+  if indent and (indent.expandtab ~= nil or indent.shiftwidth ~= nil) then
     M.set_indent({
       global = false,
-      unpack(indent),
+      options = indent,
+    })
+  elseif fallback then
+    M.set_indent({
+      global = false,
+      options = fallback,
     })
   end
 end
