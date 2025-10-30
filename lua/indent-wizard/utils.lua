d5dc09e22e41ffa4df0044162ed8faa31339456f
Charles Dong <chardon_cs@proton.me>
2025-10-30 20:18:49 +0800

    fix: settings priority

--- lua/indent-wizard/utils.lua
+++ lua/indent-wizard/utils.lua
@@ -5,11 +5,22 @@
 ---
 ---@return Settings
 function MergeSettings(settings, user_settings)
+  local out_settings = {}
+
   for key, value in pairs(user_settings) do
     if settings[key] == nil then
-      settings[key] = value
+      out_settings[key] = value
+    else
+      out_settings[key] = settings[key]
     end
   end
 
-  return settings
+  if settings.spaces ~= nil then
+    -- Restore related options
+    out_settings.shiftwidth = settings.shiftwidth
+    out_settings.tabstop = settings.tabstop
+    out_settings.softtabstop = settings.softtabstop
+  end
+
+  return out_settings
 end
